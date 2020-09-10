71cd453c7fb7192a60d018177ec20c8e816e94ed
王俊坤 <920819994@qq.com>
2020-09-10 17:27:32 +0800

    添加ui插件

--- ts_vue_exercise/src/main.ts
+++ ts_vue_exercise/src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import NutUI from '@nutui/nutui';
+import '@nutui/nutui/dist/nutui.css';
+
+Vue.use(NutUI)
 
 Vue.config.productionTip = false
 
--- ts_vue_exercise/src/views/Home.vue
+++ ts_vue_exercise/src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    我是首页
+    <div class="search">
+      <nut-searchbar
+        v-model="searchVal"
+        placeText="搜索"
+        customClass="searchInput"
+        searchBtnIconColor="#ffffff"
+      ></nut-searchbar>
+    </div>
+    <div>
+      <nut-tab @tab-switch="tabSwitch" :line-width="20">
+        <nut-tab-panel tab-title="乐库">乐库</nut-tab-panel>
+        <nut-tab-panel tab-title="推荐">推荐</nut-tab-panel>
+        <nut-tab-panel tab-title="视频">视频</nut-tab-panel>
+        <nut-tab-panel tab-title="排行榜">排行榜</nut-tab-panel>
+      </nut-tab>
+    </div>
   </div>
 </template>
 
@@ -8,6 +23,38 @@
 import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class Home extends Vue {
-  
+  searchVal = "";
+  tabSwitch(e: string): void {
+    console.log(e, 'e');
+  }
 }
 </script>
+<style lang="less" scoped>
+.search {
+  width: 100%;
+  height: 50px;
+  box-sizing: border-box;
+  padding: 10px 20px 0;
+  background: linear-gradient(to right, #34a8aa, #009697);
+  .searchInput {
+    height: 100%;
+  }
+}
+/deep/ .nut-tab {
+  padding: 0;
+  border: none;
+  background: linear-gradient(to right, #34a8aa, #009697);
+  .nut-tab-link {
+    color: #fff;
+  }
+  .nut-tab-active a {
+    color: #fff;
+    font-size: 16px;
+  }
+  .nut-tab-title,
+  .nut-tab-active,
+  .nut-title-nav-list {
+    background: transparent;
+  }
+}
+</style>
