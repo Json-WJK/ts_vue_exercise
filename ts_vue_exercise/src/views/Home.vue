685a4371664846d106a72ad03fb9f559ac3819cb
王俊坤 <920819994@qq.com>
2020-09-10 15:26:35 +0800

    创建项目

---
+++ ts_vue_exercise/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    我是首页
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Home extends Vue {
+  
+}
+</script>
