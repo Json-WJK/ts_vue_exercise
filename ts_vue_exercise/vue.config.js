8a8140c07c63ab8dbbb16ef1d1ae4bf428af8457
王俊坤 <920819994@qq.com>
2020-09-16 16:25:15 +0800

    添加前端接口代理

--- ts_vue_exercise/vue.config.js
+++ ts_vue_exercise/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     proxy: { // 配置跨域
       '/v1': {
         //要访问的跨域的api的域名
-        target: 'http://localhost:3000/v1/',
+        target: 'http://noname.wang:3000/v1/',
         ws: true,
         changOrigin: true,
         pathRewrite: {
